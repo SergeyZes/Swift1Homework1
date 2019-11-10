4bad6ecb7df2d7dd7ea107ccb02a277ce24301c8
ZesMac <sergey_the_great@mail.ru>
2019-11-10 12:18:26 +0500

    Сделан урок 1

--- Swift1Lesson1.playground/Contents.swift
+++ Swift1Lesson1.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
+//Задание 1: уравнение ax2 + bx + c = 0
+
+let a = 2.0
+let b = 8.0
+let c = 5.0
+
+let d = b * b - 4.0 * a * c
+
+if a == 0 {
+    print("Это не квадратное уравнение")
+} else if d < 0 {
+    print ("Нет решения")
+} else if d == 0 {
+    let x1 = -b  / 2 / a
+    print ("Уравнение имеет одно решение x = \(x1)")
+} else {
+    let x1 = (-b - sqrt(d)) / 2 / a
+    let x2 = (-b + sqrt(d)) / 2 / a
+    print ("Уравнение имеет два решения x1 = \(x1) и x2 = \(x2)")
+}
+
+
+
